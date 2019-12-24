493072b08c72a698418a766ee8408f9ab11e1422
Kalaitsidis Arslan <dimitrijchannel@gmail.com>
2019-12-24 15:19:28 +0200

    Changed index.php -- added html elements needed for the game's table

--- index.php
+++ index.php
@@ -2,8 +2,58 @@
     require_once "header.php"
 ?>
 
-<div class="row justify-content-center content">
 
+<div class="row justify-content-center content disappear">
+    <div class="col-12 table mt-2">
+        <div class="row computer ">
+            <div class="col-12 d-flex justify-content-center" >
+                <div class="card w-25 ">
+                    <div class="card-header">
+                        Computer
+                    </div>
+                    <div class="card-body">
+                        <p class="text-left computer-status">Game Status:</p>
+                        <p class="text-left computer-points">Computer Points:</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row splitter">
+
+        </div>
+        <div class="row players card-deck mt-5 players">
+            <div class="card disappear player">
+                <div class="card-header text-center player_name">
+                    Player1
+                </div>
+                <div class="card-body ">
+                    <p class="text-left status">Status:</p>
+                    <p class="text-left money">Money:</p>
+                    <p class="text-left points">Points:</p>
+                </div>
+            </div>
+            <div class="card disappear player">
+                <div class="card-header text-center player_name">
+                    Player1
+                </div>
+                <div class="card-body ">
+                    <p class="text-left status">Status:</p>
+                    <p class="text-left money">Money:</p>
+                    <p class="text-left points">Points:</p>
+                </div>
+            </div>
+            <div class="card disappear player">
+                <div class="card-header text-center player_name">
+                    Player1
+                </div>
+                <div class="card-body ">
+                    <p class="text-left status">Status:</p>
+                    <p class="text-left money">Money:</p>
+                    <p class="text-left points">Points:</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php
